b50945209a43005db60fdc97a6ac38e8422cf00f
Daniela Flores <kenzie85@hotmail.com>
2018-07-06 14:13:16 -0500

    Formulario CDMX

--- sendemail.php
+++ sendemail.php
@@ -1,32 +1,43 @@
 <?php
 
-// Define some constants
-define( "RECIPIENT_NAME", "Franco Equinoterapia" );
-define( "RECIPIENT_EMAIL", "kenzie85@hotmail.com" );
+	// Read the form values
+	$success = false;
 
+	// Save data from the form
+	$from = isset( $_POST['email'] ) ? preg_replace( "/[^\.\-\_\@a-zA-Z0-9]/", "", $_POST['email'] ) : "";
+	$to = 'danflovier@gmail.com';
+	$subject = isset( $_POST['subject'] ) ? preg_replace( "/[^\.\-\' a-zA-Z0-9]/", "", $_POST['subject'] ) : "";
+	$message = isset( $_POST['message'] ) ? preg_replace( "/(From:|To:|BCC:|CC:|Subject:|Content-Type:)/", "", $_POST['message'] ) : "";
+	$name = isset( $_POST['username'] ) ? preg_replace( "/[^\.\-\' a-zA-Z0-9]/", "", $_POST['username'] ) : "";
+	$phone = isset( $_POST['phone'] ) ? preg_replace( "/[^0-9]/", "", $_POST['phone'] ) : "";
 
-// Read the form values
-$success = false;
-$senderName = isset( $_POST['username'] ) ? preg_replace( "/[^\.\-\' a-zA-Z0-9]/", "", $_POST['username'] ) : "";
-$senderEmail = isset( $_POST['email'] ) ? preg_replace( "/[^\.\-\_\@a-zA-Z0-9]/", "", $_POST['email'] ) : "";
-$senderPhone = isset( $_POST['phone'] ) ? preg_replace( "/[^\.\-\_\@a-zA-Z0-9]/", "", $_POST['phone'] ) : "";
-$subject = isset( $_POST['subject'] ) ? preg_replace( "/[^\.\-\' a-zA-Z0-9]/", "", $_POST['subject'] ) : "";
-$message = isset( $_POST['message'] ) ? preg_replace( "/(From:|To:|BCC:|CC:|Subject:|Content-Type:)/", "", $_POST['message'] ) : "";
+	// If values exist, send e-mail
+	if ( $name && $from && $phone && $subject && $message) {
+		// Create header
+		$headers = "From: " . $name . " <" . $from . ">";
+		// Create the body of the message
+		$message_body = "Nombre: " . $name ."\nCorreo: " . $from . "\nTelefono: " . $phone ."\nMensaje: " . $message . "";
+		// Save if the submission of the e-mail was successful
+		$success = mail( $to, $subject, $message_body, $headers);
 
-// If all values exist, send the email
-if ( $senderName && $senderEmail && $senderPhone && $subject && $message) {
-  $recipient = RECIPIENT_NAME . " <" . RECIPIENT_EMAIL . ">";
-  $headers = "From: " . $senderName . " <" . $senderEmail . ">";
-  $msgBody = " Phone: " . $senderPhone ." Subject: " . $subject ." Message: " . $message . "";
-  $success = mail( $recipient, $headers, $msgBody );
-
-  //Set Location After Successsfull Submission
-  header('Location: cdmx-contacto.html?message=success');
-}
-
-else{
-	//Set Location After Unsuccesssfull Submission
-  	header('Location: cdmx-inicio.html?message=unsuccessfull');	
-}
+		if ($success){
+			// Set alert after successsfull submission
+			echo "<script> alert('¡Mensaje enviado exitosamente!'); </script>";
+		}else{
+			// Set alert after unsuccesssfull submission
+			echo "<script> alert('ERROR. Mensaje no enviado. Intente nuevamente o más tarde.'); </script>";
+		}
+		// Set location
+		echo "<script> window.location.replace('cdmx-contacto.html'); </script>";
+	}
+	else{
+		//Set alert and location after unsuccesssfull submission
+		echo "
+			<script> 
+				alert('¡ERROR! Rellene los campos correctamente');
+				window.location.replace('cdmx-contacto.html');
+			</script>
+			";
+	}
 
 ?>
